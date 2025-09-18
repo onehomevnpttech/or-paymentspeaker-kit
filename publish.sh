85953a6a5687aa43ca844569923f1b395faba79b
ducdt1 <ducdt1@vnpt-technology.vn>
2025-09-18 11:23:36 +0700

    slkdjhklasjhdg

--- publish.sh
+++ publish.sh
@@ -1,1 +1,17 @@
-pod repo push git@onehomevnpttech.github.com:onehomevnpttech/stc-podspecs.git ORPaymentSpeakerKit.podspec --allow-warnings
+# Sources
+read -p "Enter commit message: " message
+git add . && git commit -m ${message}
+#git push origin
+
+
+# Tag
+read -p "Enter git tag: " tag
+git tag -d ${tag}
+git tag ${tag}
+echo "Data"
+echo ${message}
+echo ${tag}
+#git push --tags -f
+
+## Cocoapods
+#pod repo push git@onehomevnpttech.github.com:onehomevnpttech/stc-podspecs.git ORPaymentSpeakerKit.podspec --allow-warnings
